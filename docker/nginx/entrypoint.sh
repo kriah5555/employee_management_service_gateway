4c8ad85df6f4518260238b4f31e0540470933730
vishal <vishal.dudalkar@infanion.com>
2023-08-02 17:38:28 +0530

    changes

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-envsubst '${SERVICE_GATEWAY_SERVER_NAME}' < /etc/nginx/conf.d/servicegateway.conf.template > /etc/nginx/conf.d/servicegateway.conf
-
-envsubst '${MASTER_DATA_SERVER_NAME}' < /etc/nginx/conf.d/masterdata.conf.template > /etc/nginx/conf.d/masterdata.conf
-
-envsubst '${IDENTITY_MANAGER_SERVER_NAME}' < /etc/nginx/conf.d/identitymanager.conf.template > /etc/nginx/conf.d/identitymanager.conf
+envsubst '${SERVICE_GATEWAY_SERVER_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx
 exec nginx -g 'daemon off;'
